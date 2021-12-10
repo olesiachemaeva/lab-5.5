c203333eb5454ab33ff7bbf4f943083360cb4ab2
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2021-12-10 12:12:15 +1000

    Добавьте файлы проекта.

---
+++ 5.5.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._5._2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+            
+        {
+            int[] array = new int[4];
+                            for (int i = 0; i < (0); i++)
+                    for (int j = 0; j < (1); j++)
+                {
+                    Console.WriteLine(array[i]);
+                    Console.WriteLine(array[j]);
+                        //= (i + j) % 2 == 1 ? 1 : 0);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+    
+
---
+++ lab 5.5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            { int[,] f = new int[4, 4];
+                for (int i = 0; i < 4;i++)
+                {
+                    for(int j=0; j<4; j++)
+                    {
+                        if ((i + j) % 2 == 1)
+                            f[i, j] = 1;
+                        else { f[i, j] = 0; }
+                                datagridvieewl[i, j].Value = f[i, j];
+                        
+                        
+                    }
+                }
+            }
+        }
+    }
+}
